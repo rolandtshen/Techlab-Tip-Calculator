fab786938a86058d5c286b86d053c1cabef1ce4d
EpicPandaLazer <techlab@mini27.local>
2018-06-29 16:29:43 -0700

    init

---
+++ TipCalculatorStarter/ColorTheme.swift
@@ -0,0 +1,42 @@
+//
+//  ColorTheme.swift
+//  TipCalculatorStarter
+//
+//  Created by pawn on 6/29/18.
+//  Copyright © 2018 Make School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct ColorTheme {
+    static let light = ColorTheme(isDefaultStatusBar: true,
+   
+                                  viewControllerBackgroundColor: .tcOffWhite,
+                                  primaryColor: .tcWhite,
+                                  primaryTextColor: .tcCharcoal,
+                                  secondaryColor: .tcDarkBlue,
+                                  accentColor: .tcHotPink,
+                                  outputTextColor: .tcAlmostBlack)
+    
+    static let dark = ColorTheme(isDefaultStatusBar: false,
+                                 viewControllerBackgroundColor: .tcAlmostBlack,
+                                 primaryColor: .tcMediumBlack,
+                                 primaryTextColor: .tcWhite,
+                                 secondaryColor: .tcBlueBlack,
+                                 accentColor: .tcSeafoamGreen,
+                                 outputTextColor: .tcWhite)
+
+    // MARK: - Instance Vars
+    
+    let isDefaultStatusBar: Bool
+    let viewControllerBackgroundColor: UIColor
+    
+    let primaryColor: UIColor
+    let primaryTextColor: UIColor
+    
+    let secondaryColor: UIColor
+    
+    let accentColor: UIColor
+    let outputTextColor: UIColor
+}
